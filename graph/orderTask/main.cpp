de4a65ac6cd42370d3e5bfd28a9c4270909ad8c7
jiaxin96 <572673807@qq.com>
2016-12-09 18:11:40 +0800

    orderTeskFinished

--- graph/orderTask/main.cpp
+++ graph/orderTask/main.cpp
@@ -1,7 +1,9 @@
 #include <cstdio>
 #include <vector>
+#include <set>
 #include <list>
 using namespace std;
+void DFS(vector< set<int, greater<int> > > &mm, vector<int> & visit, int num, list<int> & ans);
 
 int main(int argc, char const *argv[])
 {
@@ -10,13 +12,60 @@ int main(int argc, char const *argv[])
 	while (t--) {
 		int n, m;
 		scanf("%d%d", &n, &m);
-		vector< list<int> > mm(n);
+		vector< set<int, greater<int> > > mm(n+1);
+		vector<int> visit(n+1, 0);
+		list<int> ans;
 		int p1,p2;
 		for (size_t i = 0; i < m; i++)
 		{
 			scanf("%d%d", &p1, &p2);
-			
+			mm[p1].insert(p2);
 		}
+		for (size_t i = n; i >= 1; i--)
+		{
+			if (mm[i].empty())
+			{
+				if (visit[i] > 0) continue;
+				else
+				{
+					visit[i]++;
+					ans.push_front(i);
+				}
+			}
+			else
+			{
+				if (!visit[i]) {
+					visit[i]++;
+					DFS(mm, visit, i, ans);
+				}
+			}
+		}
+		for (list<int>::iterator it = ans.begin(); it != ans.end(); it++)
+		{
+			printf("%d ", *it);
+		}
+		printf("\n");
 	}
 	return 0;
 }
+void DFS(vector< set<int, greater<int> > > &mm, vector<int> & visit, int num, list<int> & ans) {
+	for (set<int, greater<int> >::iterator it = mm[num].begin(); it != mm[num].end(); it++)
+	{
+		if (!visit[*it]) {
+			visit[*it]++;
+			DFS(mm, visit, *it, ans);
+		}
+	}
+	ans.push_front(num);
+}
+
+
+
+
+
+
+
+
+
+
+
